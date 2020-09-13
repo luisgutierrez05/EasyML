955f6fbe62c0b4281efa35b191339993f41797fe
Luis <luisggbl2005@gmail.com>
2020-09-13 17:02:56 +0200

    Added __repr__ for the model class and created skeleton for build and forward methods.

--- Networks.py
+++ Networks.py
@@ -1,1 +1,19 @@
 import Layers
+import numpy as np
+
+class Model:
+    def __init__(self, layers, input_shape = None):
+        self.layers = layers
+
+    def __repr__(self):
+        res = """"""
+        for layer in self.layers:
+            res += layer.__repr__() + "\n"
+        return res
+
+    def build(self):
+        for layer in layers:
+            layer.build()
+
+    def forward(self, inputs):
+        pass
