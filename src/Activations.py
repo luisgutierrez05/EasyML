19da02705fd67d7563d21433384755e4d27c70ff
Luis <luisggbl2005@gmail.com>
2020-09-16 18:26:50 +0200

    Made Relu  return numpy arrays instead of normal lists

--- src/Activations.py
+++ src/Activations.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 class Relu:
     def forward(x):
         return [max(0, y) for y in x]
     def backwards(x):
-        return [1 if y > 0 else 0 for y in x]
+        return np.array([1 if y > 0 else 0 for y in x])
