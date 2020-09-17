89a17e0739c60586db6400601917916d16d6ab2c
Luis <luisggbl2005@gmail.com>
2020-09-17 22:22:29 +0200

    Added Identity Activation

--- src/Activations.py
+++ src/Activations.py
@@ -2,6 +2,12 @@ import numpy as np
 
 class Relu:
     def forward(x):
-        return [max(0, y) for y in x]
+        return np.array([max(0, y) for y in x])
     def backwards(x):
         return np.array([1 if y > 0 else 0 for y in x])
+
+class Identity_Activation:
+    def forward(x):
+        return x
+    def backwards(x):
+        return np.ones_like(x)
