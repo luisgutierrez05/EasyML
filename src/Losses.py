d8450802bea68909ed653866b9ed20a6d42fde64
Luis <luisggbl2005@gmail.com>
2020-09-17 22:21:08 +0200

    Fixed derivative of mse

--- src/Losses.py
+++ src/Losses.py
@@ -2,4 +2,4 @@ class MeanSquaredError:
     def forward(self, y, yhat):
         return (y - yhat) ** 2
     def backward(self, y, yhat):
-        return 2 * (y - yhat)
+        return -2 * (y - yhat)
